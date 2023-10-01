49ecbea5c05aa518b9d637a3b80f103f46192f34
rrr <rrr@yandex.ru>
2023-10-01 15:17:04 +0300

    Init

---
+++ kafka-basics/src/main/java/kafka/ProducerDemo.java
@@ -0,0 +1,21 @@
+package kafka;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ProducerDemo {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        log.warn("from debug");
+
+        //create Producer Properties
+
+        //create the Producer
+
+        //send data
+
+        //flush and close the Producer
+    }
+}
