1e3fb1e7ecd2939a0188f4a79f2f43ca38134ec8
rrr <rrr@yandex.ru>
2023-10-03 16:24:34 +0300

    First kafka producer

--- kafka-basics/src/main/java/kafka/ProducerDemo.java
+++ kafka-basics/src/main/java/kafka/ProducerDemo.java
@@ -1,6 +1,12 @@
 package kafka;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
 
 @Slf4j
 public class ProducerDemo {
@@ -11,11 +17,27 @@ public class ProducerDemo {
         log.warn("from debug");
 
         //create Producer Properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
         //create the Producer
+        KafkaProducer<String,String> producer = new KafkaProducer<>(properties);
 
-        //send data
+        //create a producer record
+        ProducerRecord<String,String> producerRecord =
+                new ProducerRecord<>("demo_java", "another hello from java code");
+
+        //send the data - async operation
+        producer.send(producerRecord);
+
+        //flush data - sync operation (not necessary)
+        producer.flush();
 
         //flush and close the Producer
+        producer.close();
+
+
     }
 }
